873691d1a038ac93af2b9f0abdeae83ab2319a62
Yair Szarf <yair@mac.com>
2016-04-16 12:04:37 -0400

    Adding files for Visa Transactions

---
+++ HackathonMusic/VisaAPI.h
@@ -0,0 +1,13 @@
+//
+//  VisaAPI.h
+//  HackathonMusic
+//
+//  Created by Yair Szarf on 4/16/16.
+//  Copyright © 2016 Everett Gilmore. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface VisaAPI : NSObject
+
+@end
---
+++ HackathonMusic/VisaAPI.m
@@ -0,0 +1,13 @@
+//
+//  VisaAPI.m
+//  HackathonMusic
+//
+//  Created by Yair Szarf on 4/16/16.
+//  Copyright © 2016 Everett Gilmore. All rights reserved.
+//
+
+#import "VisaAPI.h"
+
+@implementation VisaAPI
+
+@end
